a5ca7e8ec3bf578e8c3940246b2c530346ad315e
Chris <cdwetal@gmail.com>
2020-12-17 20:36:01 -0800

    Initial commit

---
+++ TradrORM/TradrORM/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradrORM
+{
+    public class Price
+    {
+        public int ID { get; set; }
+        public Stock Stock { get; set }
+        public DateTime DateTime { get; set; }
+        public decimal Open { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Close { get; set; }
+        public int Volume { get; set; }
+        public int VolumeAdjusted { get; set }
+
+        public Price() { }
+
+        public enum Interval
+        {
+            1Min = 0,
+            5Min = 1
+        }
+
+        public Price(int id, Stock stock, DateTime dateTime, decimal open, decimal high, decimal low, decimal close, int volume, int volumeAdjusted)
+        {
+            this.ID = id;
+            this.Stock = stock;
+            this.DateTime = dateTime;
+            this.Open = open;
+            this.High = high;
+            this.Low = low;
+            this.Close = close;
+            this.Volume = volume;
+            this.VolumeAdjusted = volumeAdjusted
+        }
+
+        public static Price Get(int id)
+        {
+            return null;
+        }
+
+        public static bool Save(Price price)
+        {
+            return true;
+        }
+    }
+}
---
+++ TradrORM/TradrORM/Stock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradrORM
+{
+    public class Stock
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public int IPOYear { get; set; }
+        public string Sector { get; set; }
+        public string Industry { get; set; }
+
+        public Stock() { }
+
+        public Stock(string symbol, string name, int ipoYear, string sector, string industry)
+        {
+            this.Symbol = symbol;
+            this.Name = name;
+            this.IPOYear = ipoYear;
+            this.Sector = sector;
+            this.Industry = industry;
+        }
+
+        public static Stock Get(int id)
+        {
+            return null;
+        }
+
+        public static bool Save(Stock stock)
+        {
+            return true;
+        }
+    }
+}
